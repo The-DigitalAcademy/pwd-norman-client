0c7a175da666a7a66d509676dfb60f104124529b
Bonke12 <116150685+Bonke12@users.noreply.github.com>
2023-08-15 13:10:06 +0200

    Cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import "../Css/Header.css";
 import Menu from "./Menu";
 import winelogo from "../assets/wine-logo.jpg"
+import Cart from "../pages/Cart";
 
 function Header() {
   return (
@@ -38,9 +39,11 @@ function Header() {
         <li className='item'><a href="/menu">Menu</a></li>
         <li className='item'><a href="/contact">Contact</a></li>
         <li className='item'><a href="/about">About</a></li>
+        <li className='item'><a href="/about">Cart</a></li>
         <li className='item button'><a href="/Login">Login</a></li>
         <li className='item button secondary'><a href="/register">Sign Up</a></li>
         <li className='toggle'><a href="#"><span className="bars"></span></a></li>
+       
 
       </ul>
     </nav>
