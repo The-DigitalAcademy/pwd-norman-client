bbe63862b846d0b461e59a3e06059d2b0bd796de
sonke06 <sonkemlambo068@gmail.com>
2023-08-18 11:00:54 +0200

    contact added

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../pages/Footer';
-import '../Css/ContactUs.css'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../pages/Footer";
+import "../Css/ContactUs.css";
 
 const ContactUs = () => {
   const handleSubmit = (event) => {
@@ -11,20 +11,74 @@ const ContactUs = () => {
 
   return (
     <div>
-      <Header />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
-
-        <label htmlFor="email">Email address:</label>
-        <input type="email" id="email" name="email" />
-
-        <label htmlFor="subject">Subject:</label>
-        <input type="text" id="subject" name="subject" />
-
-        <button type="submit">Submit</button>
-      </form>
-      <Footer />
+      <Header/>
+      <section className="contact">
+        <div className="content">
+          <h2>Contact Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            nihil odit adipisci illo inventore eum, corrupti commodi delectus.
+          </p>
+        </div>
+        <div className="container">
+          <div className="contactInfo">
+            <div className="box">
+              <div className="icon">
+                <i className="fa fa-map-marker" aria-hidden="true"></i>
+              </div>
+              <div className="text">
+                <h3>Address</h3>
+                <p>
+                  1234 Pachora Road,
+                  <br />
+                  Pune,India,
+                  <br />
+                  14568
+                </p>
+              </div>
+            </div>
+            <div className="box">
+              <div className="icon">
+                <i className="fa fa-phone" aria-hidden="true"></i>
+              </div>
+              <div className="text">
+                <h3>Phone</h3>
+                <p>12345678</p>
+              </div>
+            </div>
+            <div className="box">
+              <div className="icon">
+                <i className="fa fa-envelope-o" aria-hidden="true"></i>
+              </div>
+              <div className="text">
+                <h3>Email</h3>
+                <p>abc@gmail.com</p>
+              </div>
+            </div>
+          </div>
+          <div className="contactForm">
+            <form>
+              <h2>Send Message</h2>
+              <div className="inputBox">
+                <input type="text" required="required" />
+                <span>Full Name</span>
+              </div>
+              <div className="inputBox">
+                <input type="text" required="required" />
+                <span>Eamil</span>
+              </div>
+              <div className="inputBox">
+                <textarea name="" id="" required="required"></textarea>
+                <span>Type your Message...</span>
+              </div>
+              <div className="inputBox">
+                <input type="submit" value="Send" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+      <Footer/>
     </div>
   );
 };
