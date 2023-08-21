3e64b72a2ff015f97f769d7bd24afb97f5d7e176
Thozama3 <t.marasela@gmail.com>
2023-08-21 13:03:47 +0200

    Add new Article.css and Article.jsx

---
+++ src/pages/Article.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Css/Article.css'
+
+function Article() {
+  return (
+    <div className='wine'>Article
+    
+    </div>
+  )
+}
+
+export default Article
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/Header'
 import Slider from '../components/Slider'
 import Footer from './Footer'
+import Article from './Article'
 const Home = () => {
     return (
         <div>
             <Header/>
             <Slider/>
+            <Article/>
             <Footer/>
         </div>
     )
