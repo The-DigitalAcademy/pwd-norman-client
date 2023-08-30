babb84fa2545194aa0c77420479265c5da16f8f2
Bonke12 <116150685+Bonke12@users.noreply.github.com>
2023-08-30 13:17:16 +0200

    cart added

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -3,15 +3,22 @@ import '../Css/Cart.css'
 import Footer from './Footer'
 import Headers from '../components/Header'
 
+
+
+
+
+
+
+
 const Cart = () => {
   return (
-    <div>
-      <Headers/>
-      this is cart
-      <Footer/>
-    </div>
-    
-  )
-}
 
-export default Cart
+    <><Headers />
+    
+      <Footer />
+    </>  
+
+  );
+};
+
+export default Cart;
